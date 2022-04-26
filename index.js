ddea58680cbe0e7294e87351954a190e223c67e5
Tonya Dearman <dearman.tonya@gmail.com>
2022-04-26 13:08:15 -0700

    Added Modules

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 //Our imports and Express Server Declaration 
 const express = require('express');
+const visitorRoutes = require('./routes/visitors-routes.js')
+const adminRoutes = require('./routes/admin-routes.js')
 const server = express();
 server.use(express.json());
+server.use("/api/visitors", visitorRoutes)
+server.use("/api/admin", adminRoutes)
 //Our 1st Get request to the route '/'
 server.get('/', (req, res) => {
   res.send("Testing Puppy Adoption Server!!!!!!")
