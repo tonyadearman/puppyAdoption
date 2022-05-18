ed7b0dd0d46ad58e922c26c9849185de6ff00c96
StephanieKirshner <88007877+StephanieKirshner@users.noreply.github.com>
2022-05-18 15:12:18 -0500

    Add background color, “Contact us” image

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+import lazyDogs from '../../images/lazyDogs.png'
+
+const Footer = () => {
+  return (
+    <div className='flex-container footer_container'>
+        <img src={lazyDogs} alt='lazy dogs' />
+
+
+    </div>
+  )
+}
+
+export default Footer
